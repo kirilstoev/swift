419508ec45c3d263833faf3d2842e7385a7b277d
Kiril Stoev <Kiril.Stoev@jda.com>
2017-02-25 16:21:10 +0200

    First commit

---
+++ 6_OOPPrinciples_Debugging/src/pkg6_oopprinciples_debugging/Credentials.java
@@ -0,0 +1,11 @@
+
+package pkg6_oopprinciples_debugging;
+
+public class Credentials {
+
+private String username;
+private String password;
+
+
+
+}
---
+++ 6_OOPPrinciples_Debugging/src/pkg6_oopprinciples_debugging/Task2_CredentialsManager.java
@@ -0,0 +1,9 @@
+package pkg6_oopprinciples_debugging;
+
+public class Task2_CredentialsManager {
+
+    public static void main(String[] args) {
+
+    }
+
+}
