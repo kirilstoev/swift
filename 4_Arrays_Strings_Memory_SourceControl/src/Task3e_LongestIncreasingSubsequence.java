694584b97564266bcac2059f7d96c91a5d1e134b
Kiril Stoev <Kiril.Stoev@jda.com>
2017-03-09 22:19:50 +0200

    Fixes v1.0

--- 4_Arrays_Strings_Memory_SourceControl/src/Task3e_LongestIncreasingSubsequence.java
+++ 4_Arrays_Strings_Memory_SourceControl/src/Task3e_LongestIncreasingSubsequence.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Task3e_LongestIncreasingSubsequence {
 
     public static void main(String[] args) {
-String word = "Peter;Petrov;M;1985;89.9;184;miner;4.5;4;3.75;4.25";
-        System.out.println(word.split(";"));
+String word = "Peter,Petrov,M,1985,89.9,184,miner,4.5,4,3.75,4.25";
+        System.out.println(word.split(","));
     }
 }
